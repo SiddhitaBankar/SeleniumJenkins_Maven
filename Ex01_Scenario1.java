c26fa442bc88d6f1ee0b58159111ffa9850d12fc
SiddhitaBankar <134680214+SiddhitaBankar@users.noreply.github.com>
2023-05-29 16:27:49 +0530

    Add files via upload

---
+++ Ex01_Scenario1.java
@@ -0,0 +1,28 @@
+package ex01_stepdefinitions_multiplescenarios_way1;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+public class Ex01_Scenario1 {
+
+	
+	@Given("Chrome is opened and PB app is opened")
+	public void chrome_is_opened_and_PB_app_is_opened() {
+
+	}
+	
+	@When("User enters correct Username and Password on chrome")
+	public void user_enters_correct_Username_and_Password_on_chrome() {
+		
+	}
+	
+	@When("User clicks on login button on chrome")
+	public void user_clicks_on_login_button_on_chrome() {
+		
+	}
+	@Then("It shows home page on chrome")
+	public void it_shows_home_page_on_chrome() {
+}
+}
